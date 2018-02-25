aa169af2dbf7081d7c1f0ae585726cd0bfd41a29
Daniel Santamaría <santaka87@gmail.com>
2018-02-25 19:49:33 +0100

    install phpunit bridge and configure phpunit and travis

---
+++ Tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DDDominio\EventSourcingBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
